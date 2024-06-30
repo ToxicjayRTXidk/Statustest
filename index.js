fd8d1e5da9008ca43f9d1243428b12830d293cfa
ToxicjayRTXidk <rojayking25@gmail.com>
2024-06-30 12:39:33 -0500

    index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function setPermanentStatus() {
 // Function to log in the bot
 async function login() {
   try {
-    await client.login(process.env.TTOKEN);
+    await client.login(process.env.TOKEN);
     console.log(`\x1b[36m%s\x1b[0m`, `|    🐇 Logged in as ${client.user.tag}`);
   } catch (error) {
     console.error('Failed to log in:', error);
